5018e6d8fc6975e3675c7986943066561ee2b504
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-03-28 22:18:49 -0300

    Agregados todos los archivos

---
+++ apps/frontend/modules/static/templates/indexSuccess.php
@@ -0,0 +1,18 @@
+<div id="indexContent">
+  <div id="leftSide">
+    <div id="slider-wrapper">
+        <div id="slider">
+            <img src="images/slider/slide1.jpg" alt="" />
+            <img src="images/slider/slide2.jpg" alt="" title="#htmlcaption" />
+            <img src="images/slider/slide3.png" alt="" title="This is an example of a caption" />
+            <img src="images/slider/slide4.png" alt="" />
+        </div>
+        <div id="htmlcaption" class="nivo-html-caption">
+            <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
+        </div>
+    </div>
+  </div>
+<!--  <div id="sideBar">
+    
+  </div>-->
+</div>
---
+++ apps/frontend/templates/layout.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta name="google-site-verification" content="pyV4smMEAh7soahMM_ExvNiAHPrkGiSbJuvSHz5DdMc" />
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-22337712-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
+  </head>
+  <body>
+    <div id="con-pos">
+      <div id="container">
+        <div id="top-bg">
+          <div id="header"></div>
+        </div>
+        <div id="menu-bg">
+          <div id="menu">
+            <ul>
+              <li class="green">
+                <p><a href=""></a></p>
+                <p class="subtext"></p>
+              </li>
+              <li class="yellow">
+                <p><a href=""></a></p>
+                <p class="subtext"></p>
+              </li>
+              <li class="red">
+                <p><a href=""></a></p>
+                <p class="subtext"></p>
+              </li>
+              <li class="blue">
+                <p><a href=""></a></p>
+                <p class="subtext"></p>
+              </li>
+              <li class="purple">
+                <p><a href=""></a></p>
+                <p class="subtext"></p>
+                <p id="facebook"><?php echo link_to(image_tag('facebook-icon-48x48.png',array('alt' => 'Encuentranos en Facebook', 'size' => '24x24','border'=> '0')),'http://www.facebook.com/mundo.cantaclaro',array('target'=>'_blank')); ?></p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="content-bg">
+
+          <div id="left">
+            <div id="left-bg"></div>
+            <div id="left-bg2"></div>
+          </div>
+          
+          <div id="center-bg">
+              <?php echo $sf_content; ?>
+          </div>
+
+          <div id="right">
+            <div id="right-bg"></div>
+            <div id="right-bg2"></div>
+          </div>
+
+        </div>
+        <div id="footer-bg">
+<!--          <div id="left-bottom-bg"></div>-->
+          <div id="center-bottom-bg"></div>
+<!--          <div id="right-bottom-bg"></div>-->
+        </div>
+        <div id="footer-menu">
+          <div id="footer"></div>
+          <div id="copyright">Developed &amp; Designed - Rodrigo Campos H.<br/><a href="http://validator.w3.org/check/referer" target="_blank" class="copyright">W3C</a></div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
