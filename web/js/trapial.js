4aa08e15cffd9c0f617545403a2715fccde40954
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-02 15:15:31 -0300

    Listo Layout, ahora voy por sideBar

--- web/js/trapial.js
+++ web/js/trapial.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $("#left-bg2").height($("#center-bg").height() - 679);
     $("#right-bg2").height($("#center-bg").height() -679);
+    $("#sideBar").height($("#center-bg").height());
 });
