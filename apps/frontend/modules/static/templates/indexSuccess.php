5fd862e10a22af8aa3bcef2e2b0cac52054753db
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-02 13:50:11 -0300

    Slider Listo, corregidos algunos errores visuales

--- apps/frontend/modules/static/templates/indexSuccess.php
+++ apps/frontend/modules/static/templates/indexSuccess.php
@@ -4,8 +4,8 @@
         <div id="slider">
             <img src="images/slider/slide1.jpg" alt="" />
             <img src="images/slider/slide2.jpg" alt="" title="#htmlcaption" />
-            <img src="images/slider/slide3.png" alt="" title="This is an example of a caption" />
-            <img src="images/slider/slide4.png" alt="" />
+            <img src="images/slider/slide3.jpg" alt="" title="This is an example of a caption" />
+            <img src="images/slider/slide4.jpg" alt="" />
         </div>
         <div id="htmlcaption" class="nivo-html-caption">
             <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
