f9d39abf15f8c5fb9faa0369eaf02de561221954
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-10 17:17:14 -0300

    Lista configuracion basica de sfGuardDoctrine

--- apps/frontend/modules/posts/templates/indexSuccess.php
+++ apps/frontend/modules/posts/templates/indexSuccess.php
@@ -4,8 +4,8 @@
 <?php foreach ($trapial_postss as $trapial_posts): ?>
   <div id="NewsWrapper">
     <p class="NewsTitle"><a href="<?php echo url_for('posts/show?id='.$trapial_posts->getId()) ?>" class="NewsTitle"><?php echo $trapial_posts->getPostTitle() ?></a></p>
-    <p class="NewsAuthor">Escrito por: <?php echo $trapial_posts->getPostAuthorId() ?></p>
     <p class="NewsDate"><?php echo $trapial_posts->getCreatedAt() ?></p>
+    <p class="NewsAuthor">Escrito por: <?php echo $trapial_posts->getPostAuthorId() ?></p>
     <p class="NewsContent"><?php echo $trapial_posts->getPostContent() ?></p>
     <!--<p class="NewsSeeComments"><a href="#" class="NewsSeeComments"><?php echo $trapial_posts->getCommentCount() ?> Comentarios</a></p>-->
   </div>
