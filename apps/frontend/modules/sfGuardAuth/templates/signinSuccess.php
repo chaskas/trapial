fe425eb4206a0b51c725c84d1e0408ac1d58b412
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-10 19:49:17 -0300

    Corregido error li menu

---
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -0,0 +1,6 @@
+<?php use_helper('I18N') ?>
+<div id="login-wrapper">
+<h1 class="News"><?php echo __('Inicie Sesi&oacute;n', null, 'sf_guard') ?></h1>
+
+<?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
+</div>
