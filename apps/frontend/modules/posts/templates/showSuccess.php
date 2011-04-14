412d0e0f0c73429c2ef0a208f692ba8abc892061
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-14 00:18:44 -0300

    Lista Template posts/show

--- apps/frontend/modules/posts/templates/showSuccess.php
+++ apps/frontend/modules/posts/templates/showSuccess.php
@@ -1,38 +1,12 @@
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $trapial_posts->getId() ?></td>
-    </tr>
-    <tr>
-      <th>Post author:</th>
-      <td><?php echo $trapial_posts->getPostAuthorId() ?></td>
-    </tr>
-    <tr>
-      <th>Post content:</th>
-      <td><?php echo $trapial_posts->getPostContent() ?></td>
-    </tr>
-    <tr>
-      <th>Post title:</th>
-      <td><?php echo $trapial_posts->getPostTitle() ?></td>
-    </tr>
-    <tr>
-      <th>Comment count:</th>
-      <td><?php echo $trapial_posts->getCommentCount() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $trapial_posts->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $trapial_posts->getUpdatedAt() ?></td>
-    </tr>
-  </tbody>
-</table>
-
-<hr />
-
-<a href="<?php echo url_for('posts/edit?id='.$trapial_posts->getId()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('posts/index') ?>">List</a>
+<div id="NewsComplete-wrapper">
+  <p class="NewsCompleteTitle"><?php echo $trapial_posts->getPostTitle() ?></p>
+  <p class="NewsDate">Publicada por <?php echo $trapial_posts->getPostAuthorId()." el ".$trapial_posts->getCreatedAt() ?></p>
+  <p class="NewsCompleteContent"><?php echo $trapial_posts->getPostContent() ?></p>
+  <!--<p class="NewsCompleteComment-link"><?php echo $trapial_posts->getCommentCount() ?> Comentarios</p>-->
+  <div id="NewsCompleteLinks-wrapper">
+    <p class="NewsCompleteBack"><a class="NewsCompleteBack" href="<?php echo url_for('posts/index') ?>">Volver</a></p>
+    <?php if ($sf_user->isAuthenticated()): ?>
+    <p class="NewsCompleteEdit"><a class="NewsCompleteEdit" href="<?php echo url_for('posts/edit?id='.$trapial_posts->getId()) ?>">Editar</a></p>
+    <?php endif; ?>
+  </div>
+</div>
