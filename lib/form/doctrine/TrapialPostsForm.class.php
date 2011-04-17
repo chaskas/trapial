8fe7a9477fd2c5ac47098988812a4efc4e934f1f
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-17 02:33:43 -0300

    Listo TinyMCE, comienza desarrollo de template posts/new

--- lib/form/doctrine/TrapialPostsForm.class.php
+++ lib/form/doctrine/TrapialPostsForm.class.php
@@ -12,7 +12,10 @@ class TrapialPostsForm extends BaseTrapialPostsForm
 {
   public function configure()
   {
-    $this->widgetSchema['post_content']= new sfWidgetFormTextareaTinyMCE(array(  'width'  => 550,'height' => 350,'config' => 'theme_advanced_disable: "anchor,image,cleanup,help"'));
+    $this->widgetSchema['post_content']= new sfWidgetFormTextareaTinyMCE(array('theme'=>'advanced', 'width'  => 550,'height' => 350,'config' =>
+        'theme_advanced_buttons1 : "mybutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,fontselect,fontsizeselect,forecolor,blockquote",
+         theme_advanced_buttons2 : "",
+         theme_advanced_buttons3 : ""'));
 
     $this->widgetSchema['created_at']= new sfWidgetFormJQueryDate(array('config' => '{showOn: "button",buttonImage: "/images/calendar.png",buttonImageOnly: true}','culture'=>'es'));
     $this->widgetSchema['created_at']->getOption('date_widget')->setOption('format', '%day%%month%%year%');
