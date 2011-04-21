22b9935ec367bffbac17f49e3c3ac3557bb3432d
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-04-21 13:46:07 -0300

    Corregido ancho y alto de tinymce en posts/new (prod)

--- lib/form/doctrine/TrapialPostsForm.class.php
+++ lib/form/doctrine/TrapialPostsForm.class.php
@@ -12,10 +12,11 @@ class TrapialPostsForm extends BaseTrapialPostsForm
 {
   public function configure()
   {
-    $this->widgetSchema['post_content']= new sfWidgetFormTextareaTinyMCE(array('theme'=>'advanced', 'config' =>
+    $this->widgetSchema['post_content']= new sfWidgetFormTextareaTinyMCE(array('theme'=>'advanced', 'width'=> 823, 'height'=> 450, 'config' =>
         'theme_advanced_buttons1 : "mybutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,fontselect,fontsizeselect,forecolor,blockquote",
          theme_advanced_buttons2 : "",
          theme_advanced_buttons3 : "",
+         theme_advanced_resizing : true,
          theme_advanced_resizing_min_width : 823,
          theme_advanced_resizing_min_height: 450'));
 
