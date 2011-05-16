32d22e47ac07f2fb2e958bb763f7079699153d4e
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-05-15 21:47:58 -0400

    Se eliminaron modulos obsoletos

--- apps/frontend/modules/category/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>Edit Trapial photo category</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
--- apps/frontend/modules/category/templates/newSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>New Trapial photo category</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
