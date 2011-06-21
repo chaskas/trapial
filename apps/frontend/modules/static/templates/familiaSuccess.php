a8774d69504ce39feb4086916dd0f13352956bc2
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-06-21 11:08:21 -0400

    corregido error en familia

--- apps/frontend/modules/static/templates/familiaSuccess.php
+++ apps/frontend/modules/static/templates/familiaSuccess.php
@@ -34,7 +34,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h2 class="titulo-nivel">Pollitos (Nivel Sala cuna Intermedia)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Pollito.png','alt=Pollitos class=icono'); ?><br/><br/>
 <?php foreach ($pollitos as $pollito): ?>
@@ -49,7 +49,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 
 <h2 class="titulo-nivel">Leones (Nivel Sala cuna Mayor)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Leon.png','alt=Leones class=icono'); ?><br/><br/>
@@ -65,7 +65,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h2 class="titulo-nivel">Cuncunas (Nivel Medio menor I)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Cuncuna.png','alt=Cuncunas class=icono'); ?><br/><br/>
 <?php foreach ($cuncunas as $cuncuna): ?>
@@ -80,7 +80,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h2 class="titulo-nivel">Ardillas (Nivel Medio menor II)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Ardilla.png','alt=Ardillas class=icono'); ?><br/><br/>
 <?php foreach ($ardillas as $ardilla): ?>
@@ -95,7 +95,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h2 class="titulo-nivel">Conejos (Nivel Medio mayor I)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Conejo.png','alt=Conejos class=icono'); ?><br/><br/>
 <?php foreach ($conejos as $conejo): ?>
@@ -110,7 +110,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h2 class="titulo-nivel">Cachorros (Nivel Medio mayor II)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Perro.png','alt=Cachorros class=icono'); ?><br/><br/>
 <?php foreach ($cachorros as $cachorro): ?>
@@ -125,7 +125,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h2 class="titulo-nivel">Semilla (Nivel Especial)</h2>&nbsp;<?php echo image_tag('niveles/PNG150/Nuez.png','alt=Semillas class=icono'); ?><br/><br/>
 <?php foreach ($semillas as $semilla): ?>
@@ -140,7 +140,7 @@
   </div>
 </div>
 <?php endforeach; ?>
-<span><a href="#top" class="top">Volver Arriba</a></span>
+<div class="cb"><span><a href="#top" class="top">Volver Arriba</a></span></div>
 
 <h1 class="titulo-equipo">Equipo de Apoyo</h1>
 <?php foreach ($apoyos as $apoyo): ?>
